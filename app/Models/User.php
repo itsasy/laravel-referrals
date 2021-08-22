97e011099a675bffa8ceebdf28a986a4a46095dc
itsasy <ale.maldo097@gmail.com>
2021-08-21 21:52:50 -0500

    Add unique referral_code users

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'referral_code'
     ];
 
     /**
@@ -41,4 +43,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function getUniqueReferralCode()
+    {
+        //Generar un código nuevo si ya existe
+        do {
+            $code = Str::random(7);
+        } while (
+            User::where('referral_code', $code)->exists()
+        );
+
+        return $code;
+    }
 }
