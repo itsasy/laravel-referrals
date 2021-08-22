f928ee651fd0d0f910c710c76e538f7600ed22d1
itsasy <ale.maldo097@gmail.com>
2021-08-21 22:16:13 -0500

    Add referral link and save in the cookie

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -8,6 +8,15 @@ class ReferralController extends Controller
 {
     public function link(Request $request, $referralCode)
     {
+        //Si en caso abriste el link de referido,
+        //se guardará el código de referido durante 7 días en una cookie.
+        if (!$request->hasCookie('referral')) {
+            $minutes = 60 * 40 * 7;
+            $cookie = cookie('referral', $referralCode, $minutes);
 
+            return redirect('/home')->withCookie($cookie);
+        }
+
+        return redirect('/home');
     }
 }
